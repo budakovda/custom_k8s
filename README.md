5cdc4661d2f1a126bb77bbd0d8877ff14e311315
Dmitrii Budakov <budakov.web@gmail.com>
2021-12-14 01:28:25 +0300

    next

--- README.md
+++ README.md
@@ -1,1 +1,53 @@
-README.md
+# README.md
+
+Написать скрипт который будет выводить поды в кластере разработки, для которых удалены ветки
+
+```sh
+#repo_name
+git remote show origin
+git config --get remote.origin.url
+```
+
+```sh
+# branches to sed
+parse_branch() {
+    awk -F '/' '
+    BEGIN {printf "sed "} 
+    {
+        for (i=2; i<=NF; i++) 
+            if (NF == 2) 
+                printf "-e /"$i"/d ";
+            else if (i == 2) 
+                printf "-e /"$i"-" ;
+            else if (i != NF)
+                printf $i"-";
+            else
+                printf $i"/d "
+    }'
+}
+```
+
+```sh
+# inputs
+
+NS=<namespace>
+REPO=<custom-repo>
+
+get_branch() {
+    git branch -r -l --no-merged  | cat
+    # cat git.txt
+}
+
+get_pods() {
+    kubectl get deploy -n ${NS} -l app.kubernetes.io/name=${REPO}
+    # cat kubectl.txt
+}
+
+CUT=$(get_branch | parse_branch)
+get_pods | $CUT
+
+deployments=$(cat kubectl.txt)
+branches=$(cat git.txt  | sed  -e 's/origin\//front-sellers-/' -e 's/\//-/' -e 's/$/-server/')
+
+
+```
--- parse_branch.sh
+++ parse_branch.sh
@@ -2,8 +2,9 @@
 #./script svc-b2b-frontend front-sellers
 
 parse_branch() {
+    echo $stdin
+    echo -n "sed "
     awk -F '/' '
-    BEGIN {printf "sed "} 
     {
         for (i=2; i<=NF; i++) 
             if (NF == 2) 
@@ -19,18 +20,26 @@ parse_branch() {
 
 get_branch() {
     if ! git branch -r -l --no-merged; then
-        echo git_branch_error... >&2;
+        echo git_branch_list_error... >&2;
         kill -s TERM $TOP_PID;
     fi
 }
 
 get_pods() {
-    # if ! kubectl get deploy -n ${1} -l app.kubernetes.io/name=${2}; then
-    if ! false; then
-        echo kubectl_error... >&2;
+    if ! kubectl get deploy -n ${1} -l app.kubernetes.io/name=${2}; then
+        echo kubectl_get_pods_error... >&2;
         kill -s TERM $TOP_PID;
     fi
 }
+
+get_branch() {
+    git branch -r -l --no-merged  | cat
+}
+
+get_pods() {
+    kubectl get deploy -n ${NS} -l app.kubernetes.io/name=${REPO}
+}
+
 
 export TOP_PID=$$
 
@@ -39,17 +48,18 @@ if [ $# -ne 2 ]; then
     exit 1;
 fi
 
-# get_branch() {
-#     cat git.txt
-# }
-
-# get_pods() {
-#     cat kubectl.txt
-# }
-
 NS=$1
 REPO=$2
 
-CUT=$(get_branch | cat | parse_branch)
+arr=($(get_branch))
+num=${#arr[@]}
+if [ $num -eq 0 ]; then
+    echo empty_list_branches >&2;
+    exit 1;
+fi
+
+i=0; 
+CUT=$(while [ $i -lt $num ]; do echo ${arr[i]}; ((i++)); done | parse_branch)
+
 echo "get pods..."
-get_pods ${NS} ${REPO} | ${CUT}
+get_pods ${NS} ${REPO} | tee /dev/null | ${CUT}
